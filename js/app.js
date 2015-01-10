65b9d27083ae590db911953626fd22c35aa3af6b
Jhirley <jhirley.fonte@gmail.com>
2015-01-10 12:47:19 -0500

    added random ladybugs and Hero is now Pink-girl

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {  //jf adding x,y 
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
+    this.height = Resources.get(this.sprite).height;
+    this.width = Resources.get(this.sprite).width;
+    
   }
 
 // Update the enemy's position, required method for game
@@ -25,13 +30,18 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function() {
+var Player = function(x,y) {  //jf adding x,y 
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-pink-girl.png';
+    this.height = Resources.get(this.sprite).height;
+    this.width = Resources.get(this.sprite).width;
+
  }
 
 // Keeping the Parameter: dt, a time delta between ticks
@@ -51,11 +61,15 @@ Player.prototype.render = function() {
 // Place the player object in a variable called player
 
 function newGame(){
+    //col * 101, row * 83);  //jf pulled from engine.js
+    allEnemies = [
+    new Enemy((((Math.floor(Math.random()*5))+1) * 101) , (((Math.floor(Math.random()*3))+1) * 83)-20),  //jf I would have used a for loop but this is one line
+    new Enemy((((Math.floor(Math.random()*5))+1) * 101) , (((Math.floor(Math.random()*3))+1) * 83)-20),
+    new Enemy((((Math.floor(Math.random()*5))+1) * 101) , (((Math.floor(Math.random()*3))+1) * 83)-20)
+    ];  //jf created an array of enemies
+    player = new Player(2 * 101, 5 * 83);      //jf our valient hero;
 
-    allEnemies = [new Enemy(),new Enemy(),new Enemy()];  // created an array of enemies
-    player = new Player();      // our valient hero;
-
-    console.log(" new game works 1") //test point
+    console.log(" new game works 1") //jf test point
 }
 
 // This listens for key presses and sends the keys to your
